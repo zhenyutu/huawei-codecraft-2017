a297ed55cd371efcbe5a770c92971f8401078975
zhenyutu <1572629866@qq.com>
2017-03-08 12:44:07 +0800

    创建工程文件

---
+++ src/cn/tzy/app/Main.java
@@ -0,0 +1,11 @@
+package cn.tzy.app;
+
+/**
+ * Created by tuzhenyu on 17-3-8.
+ * @author tuzhenyu
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
