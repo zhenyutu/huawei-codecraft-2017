32db9a5d7e20fc7b400f7599a37e314df1809be7
zhenyutu <1572629866@qq.com>
2017-03-08 19:41:20 +0800

    引入标准工程格式

---
+++ src/cn/tzy/app/FIleInput.java
@@ -0,0 +1,31 @@
+package cn.tzy.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by tuzhenyu on 17-3-8.
+ * @author tuzhenyu
+ */
+public class FIleInput {
+
+    private static final String CHARSET_NAME = "UTF-8";
+    private Scanner scanner;
+
+    public FIleInput(File file){
+
+        if (file == null) throw new NullPointerException("argument is null");
+        try {
+            scanner = new Scanner(file, CHARSET_NAME);
+            scanner.useDelimiter(System.getProperty("line.separator"));
+            while (scanner.hasNext()) {
+                System.out.println(scanner.next());
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalArgumentException("Could not open " + file);
+        }
+    }
+
+}
---
+++ src/cn/tzy/app/GraphUtil.java
@@ -0,0 +1,9 @@
+package cn.tzy.app;
+
+/**
+ * Created by tuzhenyu on 17-3-8.
+ * @author tuzhenyu
+ */
+public class GraphUtil {
+
+}
