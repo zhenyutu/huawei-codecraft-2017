0b391788fb8abefcf865f6bdabf79d64f18cef69
zhenyutu <1572629866@qq.com>
2017-03-08 16:25:03 +0800

    实现了数据结构图的基本构造

--- src/cn/tzy/app/Graph.java
+++ src/cn/tzy/app/Graph.java
@@ -2,6 +2,39 @@ package cn.tzy.app;
 
 /**
  * Created by tuzhenyu on 17-3-8.
+ * @author tuzhenyu
  */
 public class Graph {
+    private int vertexNum;
+    private int edgeNum;
+    private Bag<Integer>[] adj;
+
+    public Graph(int vertexNum){
+        this.vertexNum = vertexNum;
+        this.edgeNum = 0;
+        this.adj = (Bag<Integer>[]) new Bag[vertexNum];
+
+        for (int i=0;i<vertexNum;i++){
+            adj[i] = new Bag<>();
+        }
+    }
+
+    public int getVertexNum(){
+        return vertexNum;
+    }
+
+    public int getEdgeNum(){
+        return edgeNum;
+    }
+
+    public Iterable<Integer> getAdj(int vertex){
+        return adj[vertex];
+    }
+
+    public void addEdge(int m,int n){
+        adj[m].add(n);
+        adj[n].add(m);
+        edgeNum++;
+    }
+
 }
